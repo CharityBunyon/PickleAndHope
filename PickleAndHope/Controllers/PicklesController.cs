9c17704dd3664b65e571477022862f818576e4c9
Charity Bunyon <CSEYMOU5@GMAIL.com>
2020-04-04 11:27:28 -0500

    Add project files.

---
+++ PickleAndHope/Controllers/PicklesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PickleAndHope.DataAccess;
+using PickleAndHope.Models;
+
+namespace PickleAndHope.Controllers
+{
+    [Route("api/pickles")]
+    [ApiController]
+    public class PicklesController : ControllerBase
+    {
+        PickleRepository _repository = new PickleRepository();
+
+            //api/pickles/add - if I added ("add) below in post 
+        [HttpPost]
+        public IActionResult AddPickle(Pickle pickleToAdd)
+        {
+            var existingP = _repository.GetByType(pickleToAdd.Type);
+            if (existingP == null)
+            {
+                _repository.Add(pickleToAdd);
+                return Created("", pickleToAdd);
+            }
+            else
+            {
+                var updatedPickle = _repository.Update(pickleToAdd);
+               // var updatedPickle = _repository.GetByType(pickleToAdd.Type);
+                return Ok(updatedPickle);
+                //existingP.NumberInStock += pickleToAdd.NumberInStock;
+            }
+            
+        }
+    }
+}
